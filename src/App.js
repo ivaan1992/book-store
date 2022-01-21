b666553552f4e61972dc7e31f11d20199bf7faaa
ivaan1992 <ivaan.lg.92@gmail.com>
2022-01-21 08:50:08 -0600

    Add optional changes

--- src/App.js
+++ src/App.js
@@ -1,17 +1,17 @@
-import React from 'react';
+import React from "react";
 import { Routes, Route } from 'react-router-dom';
 import Header from './components/Header';
 import Categories from './components/Categories';
 import Books from './components/Books';
 
 const App = () => (
-  <div className="App">
-    <Header />
-    <Routes>
-      <Route path="/categories" element={<Categories />} />
-      <Route path="/books" element={<Books />} />
-    </Routes>
-  </div>
+    <div className="App">
+      <Header />
+      <Routes>
+        <Route path="/categories" element={<Categories />} />
+        <Route exact path="/books" element={<Books />} />
+      </Routes>
+    </div>
 );
 
 export default App;
