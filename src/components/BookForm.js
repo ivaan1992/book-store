d2be6ba4f73ed26584cd07b4177efa8c14f88101
ivaan1992 <ivaan.lg.92@gmail.com>
2022-01-28 18:57:46 -0600

    Make changes required

--- src/components/BookForm.js
+++ src/components/BookForm.js
@@ -1,24 +1,25 @@
-import React, { useState } from 'react';
-import { useDispatch } from 'react-redux';
-import { addBook } from '../redux/books/books';
+import React, { useState } from "react";
+import { useDispatch } from "react-redux";
+import { addBook } from "../redux/books/books";
 
 const BookForm = () => {
-  const [title, setTitle] = useState('');
+  const [title, setTitle] = useState("");
   const dispatch = useDispatch();
   const handleChange = (event) => {
     setTitle(event.target.value);
   };
+  const [author, setAuthor] = useState("");
+  const handleAuthor = (event) => {
+    setAuthor(event.target.value);
+  };
   const handleAddBook = () => {
     const newBook = {
       name: title,
-      author: 'Anonymous',
+      author,
       id: Math.ceil(Math.random() * 10000),
     };
     dispatch(addBook(newBook));
   };
-
-  // const input = document.getElementById('bookInput');
-  // input.value = '';
   return (
     <>
       <div>
@@ -28,6 +29,12 @@ const BookForm = () => {
           placeholder="Book title"
           value={title}
           onChange={handleChange}
+        />
+        <input
+          type="text"
+          placeholder="Author"
+          value={author}
+          onChange={handleAuthor}
         />
         <button type="button" onClick={handleAddBook}>
           Add Book
