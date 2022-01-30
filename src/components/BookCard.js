186bc2a7b59d701eeb020b88b103bf53470b53af
ivaan1992 <ivaan.lg.92@gmail.com>
2022-01-29 19:05:25 -0600

    Finish styling

--- src/components/BookCard.js
+++ src/components/BookCard.js
@@ -10,15 +10,37 @@ const BookCard = ({ book }) => {
   };
   return (
     <li className="bookInfo d-flex">
-      <p>{book.id}</p>
       <div className="book-author">
+        <p className="category">{book.category}</p>
         <h3>{book.title}</h3>
-        <p className="author">{book.author}</p>
-
+        {/* <p className="author">{book.author}</p> */}
+        <div className="d-flex">
+          <button className="li-buttons" type="button">Comments</button>
+          <button className="li-buttons" onClick={handleRemoveBook} type="button">Remove</button>
+          <button className="li-buttons" type="button">Edit</button>
+        </div>
       </div>
-      <button className="Remove" type="button" onClick={handleRemoveBook}>
-        Remove
-      </button>
+      <div className="left d-flex">
+        <div className="porcentage">
+          <div className="wrapper" data-anim="base wrapper">
+            <div className="circle" data-anim="base left" />
+            <div className="circle" data-anim="base right" />
+          </div>
+          <div className="presentage d-flex">
+            <span className="pres">
+              50%
+            </span>
+            <span className="completed">
+              Completed
+            </span>
+          </div>
+        </div>
+      </div>
+      <div className="border d-flex">
+        <span className="completed">Current Chapter</span>
+        <span className="lesson">Chapter 17</span>
+        <button className="Remove" type="button">Update progress</button>
+      </div>
     </li>
   );
 };
