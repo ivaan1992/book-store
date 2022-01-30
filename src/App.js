a7e6718a8c70ee15143fff1e560e113d89c00bf2
ivaan1992 <ivaan.lg.92@gmail.com>
2022-01-29 21:26:42 -0600

    Make changes required

--- src/App.js
+++ src/App.js
@@ -1,14 +1,12 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import { Provider } from 'react-redux';
-import Header from './components/Header';
 import Books from './components/Books';
 import store from './redux/configureStore';
 import Categories from './components/Categories';
 
 const App = () => (
   <div className="App">
-    <Header />
     <Provider store={store}>
       <Routes>
         <Route path="/categories" element={<Categories />} />
